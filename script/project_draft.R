9c31e14679ffecc0f4cb6f9c80a5fd8d027575ff
xiongshizhao <71661671+xiongshizhao@users.noreply.github.com>
2025-03-05 21:48:16 +0000

    convert xlsx file to csv file

--- script/project_draft.R
+++ script/project_draft.R
@@ -1,9 +1,6 @@
 # Install and load necessary libraries
 if (!require("readxl")) install.packages("readxl")
-if (!require("writexl")) install.packages("writexl")
-
 library(readxl)
-library(writexl)
 
 # Data Importing and Preprocessing ----
 
@@ -16,5 +13,6 @@ rawdata_cleaned <- rawdata[, -c(2:12, 29)]
 # Filter rows where the second column is not "Neither" and the 15th column equals 4
 processed_data <- rawdata_cleaned[rawdata_cleaned[[2]] != "Neither" & rawdata_cleaned[[15]] == 4, -15]
 
-# Save the processed data to a new file
-write_xlsx(processed_data, "data/processed_data.xlsx")
+# Save the raw data and processed data to new CSV file
+write.csv(rawdata, "data/rawdata.csv", row.names = FALSE)
+write.csv(processed_data, "data/processed_data.csv", row.names = FALSE)
